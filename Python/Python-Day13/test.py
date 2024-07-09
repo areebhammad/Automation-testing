ff53686e0bba89034b8500aa689720503dc50aa8
areebhammad <areebhammad@gmail.com>
2024-07-09 19:49:29 +0530

    Python Day12&13

---
+++ Python/Python-Day13/test.py
@@ -0,0 +1,8 @@
+from unittest import TestCaseclass 
+# TryTesting(TestCase):
+
+def test_always_passes(self):
+    self.assertTrue(True)
+    
+def test_always_fails(self):
+    self.assertTrue(False)
